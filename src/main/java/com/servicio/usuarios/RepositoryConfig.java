09f8f8bd3c28c3839a77244ddc98db698bbb6dd8
danicode <danielanzawa@gmail.com>
2024-02-08 18:03:41 -0300

    Modificando servicio usuarios

--- src/main/java/com/servicio/usuarios/RepositoryConfig.java
+++ src/main/java/com/servicio/usuarios/RepositoryConfig.java
@@ -1,12 +1,14 @@
 package com.servicio.usuarios;
 
-import com.servicio.usuarios.models.entity.Role;
-import com.servicio.usuarios.models.entity.Usuario;
+import com.servicio.commons.usuarios.models.entity.Role;
+import com.servicio.commons.usuarios.models.entity.Usuario;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@EntityScan({"com.servicio.commons.usuarios.models.entity"})
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer {
 
--- src/main/java/com/servicio/usuarios/models/dao/UsuarioDao.java
+++ src/main/java/com/servicio/usuarios/models/dao/UsuarioDao.java
@@ -1,6 +1,6 @@
 package com.servicio.usuarios.models.dao;
 
-import com.servicio.usuarios.models.entity.Usuario;
+import com.servicio.commons.usuarios.models.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 //import org.springframework.data.repository.PagingAndSortingRepository;
